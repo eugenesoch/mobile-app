916dc1d1bc5c6a74e36ce32c25e1e556706c5e8c
Jevgenij Soch <110429125+eugenesoch@users.noreply.github.com>
2024-01-29 10:02:56 +0000

    Added further functionality on script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-9fe0e-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -22,13 +22,23 @@ addButtonEl.addEventListener("click", function() {
 })
 
 onValue(shoppingListInDB, function(snapshot) {
-    let itemsArray = Object.values(snapshot.val())
+    // Challenge: Change the onValue code so that it uses snapshot.exists() to show items when there are items in the database and if there are not displays the text 'No items here... yet'.
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
     
-    clearShoppingListEl()
+        clearShoppingListEl()
     
-    for (let i = 0; i < itemsArray.length; i++) {
-        appendItemToShoppingListEl(itemsArray[i])
+        for (let i = 0; i < itemsArray.length; i++) {
+        let currentItem = itemsArray[i]
+        let currentItemID = currentItem[0]
+        let currentItemValue = currentItem[1]
+        
+        appendItemToShoppingListEl(currentItem)
     }
+    } else {
+        shoppingListEl.textContent = `No items in the list yet. Start adding them now 🪄`
+    }
+    
 })
 
 function clearShoppingListEl() {
@@ -39,9 +49,44 @@ function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
 
-function appendItemToShoppingListEl(itemValue) {
-    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+
+    newEl.addEventListener("dblclick", function() {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        console.log(exactLocationOfItemInDB)
+        remove(exactLocationOfItemInDB)
+    })
+    shoppingListEl.append(newEl)
 }
+
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("add-button").click();
+    }
+  });
+
+// function appendItemToShoppingListEl(item) {
+//     let itemID = item[0]
+//     let itemValue = item[1]
+//     let newEl = document.createElement("li")
+//     newEl.textContent = itemValue
+//     shoppingListEl.append(newEl)
+//     newEl.addEventListener("dblclick", function() {
+//         let exactItemLocationInDB = ref(database, `shoppingList/${itemID}`)
+//         remove(exactItemLocationInDB)
+//     })
+// }
+
+
+
+
 
 
 

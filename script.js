59b7742630042eb415b9b27588897b452f5b688d
Jevgenij Soch <110429125+eugenesoch@users.noreply.github.com>
2024-02-01 07:10:09 +0000

    Added credits

--- script.js
+++ script.js
@@ -100,15 +100,3 @@ checkBoxEl.addEventListener("change", (event) => {
         localStorage.setItem('theme', targetTheme);
       }
 }) 
-
-toggle.onclick = function() {
-    var currentTheme = document.documentElement.getAttribute("data-theme");
-    var targetTheme = "light";
-
-    if (currentTheme === "light") {
-        targetTheme = "dark";
-    }
-
-    document.documentElement.setAttribute('data-theme', targetTheme)
-    localStorage.setItem('theme', targetTheme);
-};
